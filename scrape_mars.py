cac9e40a4911359cb95f4662fcc0c8ba8fc97225
HemaVyas <Hema@hkls-mbp.lan>
2021-06-08 08:11:38 -0400

    HW got website to open

---
+++ scrape_mars.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+
+# In[2]:
+
+def scrape_info():
+        executable_path = {'executable_path': ChromeDriverManager().install()}
+        browser = Browser('chrome', **executable_path, headless=False)
+
+
+        # In[3]:
+
+
+        url = 'https://redplanetscience.com/'
+        browser.visit(url)
+
+
+        # In[4]:
+
+
+        # for none DYNAMIC CONTENT
+        # response = requests.get(url)
+        # soup = bs(response.text, 'lxml') #html.parser
+        # print(soup.prettify())
+
+
+        # In[5]:
+
+
+        html = browser.html
+        soup = bs(html, 'lxml')
+
+
+        # In[6]:
+
+
+        #print(soup.prettify())
+
+
+        # In[7]:
+
+
+        results = soup.find_all('div', class_='list_text')
+
+
+        # In[8]:
+
+
+        news_names = []
+        for result in results:
+                news = {}
+                # Identify and return title of listing
+                title = result.find(class_='content_title').text
+                news['title'] = title
+                # Identify and return teaser of listing
+                teaser = result.find(class_='article_teaser_body').text
+                news['teaser'] = teaser
+                news_names.append(news)
+
+        #         # Print results only if title, price, and link are available
+        #         print('-------------')
+        #         print(title)
+        #         print(teaser)
+        print (news_names[0])
+
+
+        # In[9]:
+
+
+        url2 = 'https://spaceimages-mars.com/'
+        browser.visit(url2)
+        html = browser.html
+        soup2 = bs(html, 'lxml')
+
+
+        # In[10]:
+
+
+        #print(soup2.prettify())
+
+
+        # In[11]:
+
+
+        featured_image = soup2.find(class_='showimg fancybox-thumbs')
+        featured_image
+
+
+        # In[12]:
+
+
+        featured_image_url = format(featured_image.get('href'))
+        featured_image_url_final = url2 + featured_image_url
+        featured_image_url_final
+
+
+        # In[13]:
+
+
+        url3 = 'https://galaxyfacts-mars.com/'
+        tables = pd.read_html(url3)
+        #tables
+
+
+        # In[14]:
+
+
+                        # planet_facts_df = tables[0]
+                        # planet_facts_df.set_index(0,inplace= True)
+                        # planet_facts_df
+
+
+        # In[15]:
+
+
+                        #planet_facts_df.to_html
+
+
+        # In[16]:
+
+
+        url4 = 'https://marshemispheres.com/'
+        browser.visit(url4)
+        html = browser.html
+        soup4 = bs(html, 'lxml')
+
+
+        # In[17]:
+
+
+        #print(soup4.prettify())
+
+
+        # In[18]:
+
+
+        links =browser.find_by_css('a.product-item img')
+        #print (links)
+
+
+        # In[19]:
+
+
+        hemisphere_image_urls=[]
+        for i in range(len(links)):
+                hemisphere = {}
+                browser.find_by_css('a.product-item img')[i].click()
+                element=browser.links.find_by_text('Sample').first
+                hemisphere['title']=browser.find_by_css('h2.title').text
+                hemisphere['img_url']=element['href']
+                hemisphere_image_urls.append(hemisphere)
+                browser.back()
+        hemisphere_image_urls
+
+
+        # In[20]:
+
+        mars_data = {
+                "news_names1" : news_names[0],
+                "mars_image" : featured_image_url_final,
+               # "mars_table" : planet_facts_df,
+                "hemisphere_info": hemisphere_image_urls
+        }
+
+        browser.quit()
+
+        return mars_data
+
---
+++ scrape_mars_original.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+
+# In[ ]:
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[ ]:
+
+
+url = 'https://redplanetscience.com/'
+browser.visit(url)
+
+
+# # for none DYNAMIC CONTENT
+# response = requests.get(url)
+# soup = bs(response.text, 'lxml') #html.parser
+# print(soup.prettify())
+
+# In[ ]:
+
+
+html = browser.html
+soup = bs(html, 'lxml')
+
+
+# In[ ]:
+
+
+print(soup.prettify())
+
+
+# In[ ]:
+
+
+results = soup.find_all('div', class_='list_text')
+
+
+# # Error handling
+# for result in results:
+#     try:
+#         # Identify and return title of listing
+#         title = result.find(class_='content_title').text
+#         # Identify and return price of listing
+#         teaser = result.find(class_='article_teaser_body').text
+#         # Identify and return link to listing
+#         #link = result.a['href']
+# 
+#         # Print results only if title, price, and link are available
+#         if (title and teaser):
+#             print('-------------')
+#             print(title)
+#             print(teaser)
+#     except AttributeError as e:
+#         print(e)
+
+# In[ ]:
+
+
+for result in results:
+        # Identify and return title of listing
+        title = result.find(class_='content_title').text
+        # Identify and return teaser of listing
+        teaser = result.find(class_='article_teaser_body').text
+
+        # Print results only if title, price, and link are available
+        print('-------------')
+        print(title)
+        print(teaser)
+
+
+# In[ ]:
+
+
+browser.quit()
+
+
+# In[ ]:
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[ ]:
+
+
+url2 = 'https://spaceimages-mars.com/'
+browser.visit(url2)
+html = browser.html
+soup2 = bs(html, 'lxml')
+
+
+# In[ ]:
+
+
+print(soup2.prettify())
+
+
+# In[ ]:
+
+
+featured_image = soup2.find(class_='showimg fancybox-thumbs')
+featured_image
+
+
+# In[ ]:
+
+
+featured_image_url = format(featured_image.get('href'))
+featured_image_url_final = url2 + featured_image_url
+featured_image_url_final
+
+
+# In[ ]:
+
+
+browser.quit()
+
+
+# In[ ]:
+
+
+url3 = 'https://galaxyfacts-mars.com/'
+tables = pd.read_html(url3)
+tables
+
+
+# In[ ]:
+
+
+planet_facts_df = tables[0]
+planet_facts_df.set_index(0,inplace= True)
+planet_facts_df
+
+
+# planet_facts_df.to_html
+
+# In[2]:
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[18]:
+
+
+url4 = 'https://marshemispheres.com/'
+browser.visit(url4)
+html = browser.html
+soup4 = bs(html, 'lxml')
+
+
+# In[4]:
+
+
+print(soup4.prettify())
+
+
+# In[ ]:
+
+
+sidebar2 = soup4.findAll('a')
+sidebar2
+
+
+# hemisphere_link2 = []
+# hemisphere_name2 = []
+# 
+# for hems in sidebar2:
+#         name = sidebar2.find('h3').text
+#         hemisphere_name2.append(name)
+#         link = sidebar2.a['href']
+#         hemisphere_link2.append(link)
+# hemisphere_name2
+# hemisphere_link2
+
+# In[ ]:
+
+
+sidebar = soup4.find('div', class_='collapsible results')
+sidebar
+
+
+# In[ ]:
+
+
+sidebar = soup4.find('div', class_='collapsible results')
+hemisphere_link = []
+hemisphere_name = []
+
+for hems in sidebar:
+        name = sidebar.find('h3').text
+        hemisphere_name.append(name)
+        link = sidebar.a['href']
+        hemisphere_link.append(link)
+hemisphere_name
+hemisphere_link
+
+
+# #Making it a dictionary
+# 
+# #single entry
+# 
+# hem_info = {
+#     'title':''
+#     'img_url':''
+# }
+# 
+# hemisphere_image_urls = [
+
+# In[19]:
+
+
+#class="itemLink product-item"
+#browser.back()
+
+links =browser.find_by_css('a.product-item img')
+print (links)
+
+
+# In[20]:
+
+
+hemisphere_image_urls=[]
+for i in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css('a.product-item img')[i].click()
+        element=browser.links.find_by_text('Sample').first
+        hemisphere['img_url']=element['href']
+        hemisphere['title']=browser.find_by_css('h2.title').text
+        hemisphere_image_urls.append(hemisphere)
+        #print(element['href'])
+        browser.back()
+hemisphere_image_urls
+
+
+# In[ ]:
+
+
+hemisphere_image_urls = [
+    {"title": "Valles Marineris Hemisphere", "img_url": "..."},
+    {"title": "Cerberus Hemisphere", "img_url": "..."},
+    {"title": "Schiaparelli Hemisphere", "img_url": "..."},
+    {"title": "Syrtis Major Hemisphere", "img_url": "..."},
+]
+
+
